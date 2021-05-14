c63956783e4dabbfd970c0d66d0413916110ec0b
simrangarcha2000 <simranjeetkaur.garcha@dcmail.ca>
2021-05-13 20:58:25 -0400

    Adding 360 effect for 3D model

--- editorDesign/script/script.js
+++ editorDesign/script/script.js
@@ -1,48 +1,73 @@
 
-        let scene;
-
-        function init() {
-            scene = new THREE.Scene();
-            scene.background = new THREE.Color(0xdddddd);
-
-            sizes = {
-                width: 800,
-                height: 600
-            }
-
-            camera = new THREE.PerspectiveCamera(40, sizes.width/sizes.height, 1, 5000);
-
-            /*camera.rotation.y = 45/180*Math.PI;*/
-            //camera.position.x = 5;
-            camera.position.y = 2;
-            camera.position.z = 20;
-
-            hlight = new THREE.AmbientLight (0x404040, 100);
-            scene.add(hlight);
+let scene;
 
 
-            
-            //Connect to HTML canvas
-            const canvashtml = document.querySelector('.renderPro')
+function init() {
+    scene = new THREE.Scene();
+    scene.background = new THREE.Color(0xdddddd);
 
-            //Creating the GLTF Loader
+    sizes = {
+width: 800,
+height: 600
+    }
+
+    camera = new THREE.PerspectiveCamera(40, sizes.width/sizes.height, 1, 5000);
+
+    /*camera.rotation.y = 45/180*Math.PI;*/
+    //camera.position.x = 5;
+    camera.position.y = 2;
+    camera.position.z = 20;
+
+
+    //Lighting
+    hlight = new THREE.AmbientLight (0x404040, 100);
+    scene.add(hlight);
+
+
+    //Connect to HTML canvas
+    const canvashtml = document.querySelector('.renderPro')
+
+    //Controls for 360 Rotation
+    const controls = new THREE.OrbitControls(camera, canvashtml)
+    controls.enableDamping = true
+    
+
+
+    //Render -> First create canvas in HTML
+    renderer = new THREE.WebGLRenderer({
+//Connect HTML canvas
+canvas: canvashtml,
+    })
+    renderer.setSize(sizes.width, sizes.height);
 
 
 
-            //Render -> First create canvas in HTML
-            renderer = new THREE.WebGLRenderer({
-                //Connect HTML canvas
-                canvas: canvashtml,
-            })
-            renderer.setSize(sizes.width, sizes.height);
+    //GLTF Loader
+    let loader = new THREE.GLTFLoader();
+    loader.load('models/scene.gltf',function(gltf){
+pokemon = gltf.scene.children[0]
+pokemon.scale.set(0.5,0.5,0.5);
+scene.add(gltf.scene);
+renderer.render(scene,camera);
+    });
 
-            let loader = new THREE.GLTFLoader();
-            loader.load('models/scene.gltf',function(gltf){
-                pokemon = gltf.scene.children[0]
-                pokemon.scale.set(0.5,0.5,0.5);
-                scene.add(gltf.scene);
-                renderer.render(scene,camera);
-            });
-        }
+    // Animate
+    const clock = new THREE.Clock()
 
-        init();
+    const tick = () =>
+    {
+const elapsedTime = clock.getElapsedTime()
+//update Controls
+controls.update()
+// Render
+renderer.render(scene, camera)
+
+// Call tick again on the next frame
+window.requestAnimationFrame(tick)
+    }
+
+    tick()
+}
+init();
+
+
