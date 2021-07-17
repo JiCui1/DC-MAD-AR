80b6cb6bf4ae41230aef2a48c54072c189084e3b
Jiahua Cui <jiahuacui@Jiahuas-iMac.home>
2021-07-17 19:34:45 -0400

    added sign up error handling

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,20 +1,37 @@
 const User = require("../models/User");
 
+const handleErrors = (err) => {
+  let errors = { email: "", password: "" };
+  if (err.code === 11000) {
+    errors.email = "that email is already registered";
+    return errors;
+  }
+
+  //validation errors
+  if (err.message.includes("user validation failed")) {
+    Object.values(err.errors).forEach(({ properties }) => {
+      errors[properties.path] = properties.message;
+    });
+  }
+
+  return errors;
+};
+
 module.exports.signup_get = (req, res) => {
   res.render("signup", { title: "SIGN UP" });
 };
 
 module.exports.signup_post = async (req, res) => {
-
   const { email, password } = req.body;
-
 
   try {
     const user = await User.create({ email, password });
+    console.log(user._id);
     res.status(201).json({ user: user._id });
   } catch (err) {
     console.log(err);
-    res.status(400).json({});
+    const errors = handleErrors(err);
+    res.status(400).json({ errors });
   }
 };
 
